9f4222e8838e0a1f7619881e77e0f585b0f5ec27
Hung-Yun Liao <hungyunliao@gmail.com>
2016-05-26 09:24:00 -0700

    First commit

--- ACC/ViewController.swift
+++ ACC/ViewController.swift
@@ -7,12 +7,160 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    // Instance variables
+    var currentMaxAccelX: Double = 0.0 //negative
+    var currentMaxAccelXPositive: Double = 0.0
+    var currentMaxAccelY: Double = 0.0
+    var currentMaxAccelZ: Double = 0.0
+    var currentMaxRotX: Double = 0.0
+    var currentMaxRotY: Double = 0.0
+    var currentMaxRotZ: Double = 0.0
+    
+    var motionManager = CMMotionManager()
+    
+    var distanceT : Double = 0.0
+    var velocityX: Double = 0.0
+    
+    // Outlets
+    @IBOutlet var accX: UILabel?
+    
+    @IBOutlet var accY: UILabel?
+    
+    @IBOutlet var accZ: UILabel?
+    
+    @IBOutlet var maxAccX: UILabel?
+    
+    @IBOutlet var maxAccXPositive: UILabel?
+    @IBOutlet var maxAccY: UILabel?
+    @IBOutlet var maxAccZ: UILabel?
+    
+    @IBOutlet var rotX: UILabel?
+    @IBOutlet var rotY: UILabel?
+    @IBOutlet var rotZ: UILabel?
+    
+    @IBOutlet var maxRotX: UILabel?
+    @IBOutlet var maxRotY: UILabel?
+    @IBOutlet var maxRotZ: UILabel?
+    
+    @IBOutlet var velX: UILabel?
+    @IBOutlet var distance: UILabel?
+    
+    // Functions
+    @IBAction func reset() {
+        currentMaxAccelX = 0.0 //negative AccelX
+        currentMaxAccelXPositive = 0.0
+        currentMaxAccelY = 0.0
+        currentMaxAccelZ = 0.0
+        currentMaxRotX = 0.0
+        currentMaxRotY = 0.0
+        currentMaxRotZ = 0.0
+        
+        distanceT = 0.0
+        velocityX = 0.0
+    }
+    
+    
     override func viewDidLoad() {
+        
+        self.reset()
+        
+        // Set Motion Manager Properties
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.gyroUpdateInterval = 0.2
+        
+        // Recording data
+        
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
+            self.outputAccData(accelerometerData!.acceleration)
+            if(NSError != nil) {
+                print("\(NSError)")
+            }
+        })
+        
+        motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { (gyroData: CMGyroData?, NSError) -> Void in
+            self.outputRotData(gyroData!.rotationRate)
+            if (NSError != nil){
+                print("\(NSError)")
+            }
+        })
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func outputAccData(acceleration: CMAcceleration){
+        
+        accX?.text = "\(acceleration.x * 9.81)"
+        if acceleration.x > currentMaxAccelXPositive
+        {
+            currentMaxAccelXPositive = acceleration.x
+        }
+        
+        if acceleration.x < currentMaxAccelX // negative
+        {
+            currentMaxAccelX = acceleration.x
+        }
+        
+        accY?.text = "\(acceleration.y).2fg"
+        if fabs(acceleration.y) > fabs(currentMaxAccelY)
+        {
+            currentMaxAccelY = acceleration.y
+        }
+        
+        accZ?.text = "\(acceleration.z).2fg"
+        if fabs(acceleration.z) > fabs(currentMaxAccelZ)
+        {
+            currentMaxAccelZ = acceleration.z
+        }
+        
+        if Double(accX!.text!)! >= 1 || Double(accX!.text!)! <= -1 {
+            velocityX += acceleration.x * 9.81 * motionManager.accelerometerUpdateInterval
+        }
+        
+        maxAccX?.text = "\(currentMaxAccelX).2f"
+        maxAccXPositive?.text = "\(currentMaxAccelXPositive).2f"
+        maxAccY?.text = "\(currentMaxAccelY).2f"
+        maxAccZ?.text = "\(currentMaxAccelZ).2f"
+        
+        velX?.text = "\(velocityX)"
+        distanceT += velocityX * motionManager.accelerometerUpdateInterval
+        distance?.text = "\(distanceT)"
+        
+        
+    }
+    
+    func outputRotData(rotation: CMRotationRate){
+        
+        
+        rotX?.text = "\(rotation.x).2fr/s"
+        if fabs(rotation.x) > fabs(currentMaxRotX)
+        {
+            currentMaxRotX = rotation.x
+        }
+        
+        rotY?.text = "\(rotation.y).2fr/s"
+        if fabs(rotation.y) > fabs(currentMaxRotY)
+        {
+            currentMaxRotY = rotation.y
+        }
+        
+        rotZ?.text = "\(rotation.z).2fr/s"
+        if fabs(rotation.z) > fabs(currentMaxRotZ)
+        {
+            currentMaxRotZ = rotation.z
+        }
+        
+        maxRotX?.text = "\(currentMaxRotX).2f"
+        maxRotY?.text = "\(currentMaxRotY).2f"
+        maxRotZ?.text = "\(currentMaxRotZ).2f"
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
