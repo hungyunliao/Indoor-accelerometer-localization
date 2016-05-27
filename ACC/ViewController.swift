df046ae24b762fe33a7afe058bebe55bdcd850f5
Hung-Yun Liao <hungyunliao@gmail.com>
2016-05-27 00:16:50 -0700

    add parameter setup sections

--- ACC/ViewController.swift
+++ ACC/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    // System parameters setup
+    let accelerometerUpdateInterval: Double = 0.03
+    let gyroUpdateInterval: Double = 0.2
+    let calibrationTimeAssigned: Int = 100
+    let numberOfPointsForCalibration: Int = 3
+    
     // Instance variables
-    var currentMaxAccelX: Double = 0.0 //negative
+    var currentMaxAccelXNegative: Double = 0.0
     var currentMaxAccelXPositive: Double = 0.0
     var currentMaxAccelY: Double = 0.0
     var currentMaxAccelZ: Double = 0.0
@@ -22,30 +28,26 @@ class ViewController: UIViewController {
     
     var motionManager = CMMotionManager()
     
-    var distanceT : Double = 0.0
+    var distanceTraveled : Double = 0.0
     var velocityX: Double = 0.0
-    var calibrationTimes: Int = 100
+    var calibrationTimes: Int = 0
     var calibrationSum: Double = 0.0
-    var avg : Double = 0.0
+    var avg: Double = 0.0
     var isCalibrated: Bool = false
+    var smoothX: Int = 0
+    var accCaliSumX: Double = 0.0
     
     // Outlets
     @IBOutlet var accX: UILabel?
-    
     @IBOutlet var accY: UILabel?
-    
     @IBOutlet var accZ: UILabel?
-    
-    @IBOutlet var maxAccX: UILabel?
-    
+    @IBOutlet var maxAccXNegative: UILabel?
     @IBOutlet var maxAccXPositive: UILabel?
     @IBOutlet var maxAccY: UILabel?
     @IBOutlet var maxAccZ: UILabel?
-    
     @IBOutlet var rotX: UILabel?
     @IBOutlet var rotY: UILabel?
     @IBOutlet var rotZ: UILabel?
-    
     @IBOutlet var maxRotX: UILabel?
     @IBOutlet var maxRotY: UILabel?
     @IBOutlet var maxRotZ: UILabel?
@@ -55,7 +57,8 @@ class ViewController: UIViewController {
     
     // Functions
     @IBAction func reset() {
-        currentMaxAccelX = 0.0 //negative AccelX
+        
+        currentMaxAccelXNegative = 0.0
         currentMaxAccelXPositive = 0.0
         currentMaxAccelY = 0.0
         currentMaxAccelZ = 0.0
@@ -63,36 +66,36 @@ class ViewController: UIViewController {
         currentMaxRotY = 0.0
         currentMaxRotZ = 0.0
         
-        distanceT = 0.0
+        distanceTraveled = 0.0
         velocityX = 0.0
-        calibrationTimes = 100
+        calibrationTimes = calibrationTimeAssigned
         calibrationSum = 0
         avg = 0.0
         isCalibrated = false
+        smoothX = numberOfPointsForCalibration
+        accCaliSumX = 0.0
+        
     }
-    
     
     override func viewDidLoad() {
         
         self.reset()
         
         // Set Motion Manager Properties
-        
-        motionManager.accelerometerUpdateInterval = 0.1
-        motionManager.gyroUpdateInterval = 0.2
+        motionManager.accelerometerUpdateInterval = accelerometerUpdateInterval
+        motionManager.gyroUpdateInterval = gyroUpdateInterval
         
         // Recording data
-        
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
             self.outputAccData(accelerometerData!.acceleration)
-            if(NSError != nil) {
+            if NSError != nil {
                 print("\(NSError)")
             }
         })
         
         motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { (gyroData: CMGyroData?, NSError) -> Void in
             self.outputRotData(gyroData!.rotationRate)
-            if (NSError != nil){
+            if NSError != nil {
                 print("\(NSError)")
             }
         })
@@ -104,78 +107,81 @@ class ViewController: UIViewController {
     func outputAccData(acceleration: CMAcceleration){
         
         if !isCalibrated {
-            if calibrationTimes == 100 {
+            
+            if calibrationTimes == calibrationTimeAssigned {
                 maxAccXPositive?.text = String(acceleration.x)
             }
+            
             if calibrationTimes > 0 {
                 calibrationSum += acceleration.x
                 calibrationTimes -= 1
             } else {
-                maxAccX?.text = String(calibrationSum)
-                avg = calibrationSum / 100
+                maxAccXNegative?.text = String(calibrationSum)
+                avg = calibrationSum / Double(calibrationTimeAssigned)
                 isCalibrated = true
             }
-        }
-        else {
-            accX?.text = "\(acceleration.x - avg)"
-            if acceleration.x > currentMaxAccelXPositive
-            {
-                currentMaxAccelXPositive = acceleration.x
+            
+        } else {
+            
+            if smoothX != 0 {
+                accCaliSumX += acceleration.x
+                smoothX -= 1
+            } else {
+                accCaliSumX /= Double(numberOfPointsForCalibration)
+                accX?.text = "\(accCaliSumX - avg)"
+                if acceleration.x > currentMaxAccelXPositive {
+                    currentMaxAccelXPositive = acceleration.x
+                }
+                
+                if acceleration.x < currentMaxAccelXNegative { // negative
+                    currentMaxAccelXNegative = acceleration.x
+                }
+                accCaliSumX = 0.0
+                smoothX = numberOfPointsForCalibration
             }
             
-            if acceleration.x < currentMaxAccelX // negative
-            {
-                currentMaxAccelX = acceleration.x
-            }
         }
         
         accY?.text = "\(acceleration.y).2fg"
-        if fabs(acceleration.y) > fabs(currentMaxAccelY)
-        {
+        if fabs(acceleration.y) > fabs(currentMaxAccelY) {
             currentMaxAccelY = acceleration.y
         }
         
         accZ?.text = "\(acceleration.z).2fg"
-        if fabs(acceleration.z) > fabs(currentMaxAccelZ)
-        {
+        if fabs(acceleration.z) > fabs(currentMaxAccelZ) {
             currentMaxAccelZ = acceleration.z
         }
         
-        if Double(accX!.text!)! >= 1 || Double(accX!.text!)! <= -1 {
-            velocityX += acceleration.x * 9.81 * motionManager.accelerometerUpdateInterval
+        if Double(accX!.text!)! >= 0.1 || Double(accX!.text!)! <= -0.1 {
+            velocityX += Double(accX!.text!)! * 9.81 * motionManager.accelerometerUpdateInterval
         }
         
-        //maxAccX?.text = "\(currentMaxAccelX).2f"
+        //maxAccXNegative?.text = "\(currentMaxAccelXNegative).2f"
         //maxAccXPositive?.text = "\(currentMaxAccelXPositive).2f"
         maxAccY?.text = "\(currentMaxAccelY).2f"
         maxAccZ?.text = "\(currentMaxAccelZ).2f"
         
         velX?.text = "\(velocityX)"
-        distanceT += velocityX * motionManager.accelerometerUpdateInterval
-        distance?.text = "\(distanceT)"
+        distanceTraveled += velocityX * motionManager.accelerometerUpdateInterval
+        distance?.text = "\(distanceTraveled)"
         //distance?.text = "\(avg)"
-        
         
     }
     
     func outputRotData(rotation: CMRotationRate){
         
-        
         rotX?.text = "\(rotation.x).2fr/s"
-        if fabs(rotation.x) > fabs(currentMaxRotX)
-        {
+        if fabs(rotation.x) > fabs(currentMaxRotX) {
             currentMaxRotX = rotation.x
         }
         
         rotY?.text = "\(rotation.y).2fr/s"
-        if fabs(rotation.y) > fabs(currentMaxRotY)
-        {
+        if fabs(rotation.y) > fabs(currentMaxRotY) {
             currentMaxRotY = rotation.y
         }
         
         rotZ?.text = "\(rotation.z).2fr/s"
-        if fabs(rotation.z) > fabs(currentMaxRotZ)
-        {
+        if fabs(rotation.z) > fabs(currentMaxRotZ) {
             currentMaxRotZ = rotation.z
         }
         
@@ -183,15 +189,12 @@ class ViewController: UIViewController {
         maxRotY?.text = "\(currentMaxRotY).2f"
         maxRotZ?.text = "\(currentMaxRotZ).2f"
         
-        
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
