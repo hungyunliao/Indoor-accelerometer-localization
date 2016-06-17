fcb1a57e8b4a9b89c6dc171e9039945da5d91bb6
cm581781 <za955120@hotmail.com>
2016-06-16 17:52:25 -0700

    replace 0.1 with let velocityThreshold and set to lower 0.01 enlarge setScale from 5 to 100

--- ACC/MapViewController.swift
+++ ACC/MapViewController.swift
@@ -28,7 +28,7 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.backgroundColor = UIColor.blackColor()
-        mapView.setScale(5.0)
+        mapView.setScale(100.0)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updatePosition(_:)), name:"PositionChanged", object: nil)
         //self.view.backgroundColor = UIColor.greenColor()
     }
