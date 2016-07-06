ae12ced54e5ca52c166508c529cf8d1fd9214860
Hung-Yun Liao <hungyunliao@gmail.com>
2016-07-06 14:13:05 -0700

    test

--- ACC/MapDisplayView.swift
+++ ACC/MapDisplayView.swift
@@ -11,12 +11,17 @@ import UIKit
 @IBDesignable
 class MapDisplayView: UIView {
     private let gridLayer: GridLayer
+    private var gridLayer2: GridLayer
     
     override init(frame: CGRect) {
         gridLayer = GridLayer(frame: frame)
+        gridLayer2 = GridLayer(frame: frame)
+        gridLayer2.gridColor = UIColor.yellowColor()
         super.init(frame: frame)
         
+        
         self.layer.addSublayer(gridLayer)
+        self.layer.addSublayer(gridLayer2)
     }
     
     internal convenience required init?(coder aDecoder: NSCoder) {
@@ -26,11 +31,13 @@ class MapDisplayView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.layer.addSublayer(gridLayer)
+        self.layer.addSublayer(gridLayer2)
     }
     
     override var frame: CGRect {
         didSet {
             gridLayer.frame = frame
+            gridLayer2.frame = frame
         }
     }
 }
